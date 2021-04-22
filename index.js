1b92f4dda56e61bb2d2f0405e9a4de60cea3cf7a
Evan Shortiss <evanshortiss@gmail.com>
2021-04-22 12:43:27 +0100

    fix: use correct status code return structure fix: remove unecessary try/catch

--- index.js
+++ index.js
@@ -5,39 +5,27 @@ const log = require('./lib/log');
 
 module.exports = async ({ cloudevent }) => {
   if (!cloudevent) {
+    log.warn('received a request that did not contain a cloud event');
     return {
-      statusCode: 400,
+      code: 400,
       body: {
         info: 'invalid cloud event HTTP request'
       }
     };
   } else if (events.isKnownEventType(cloudevent)) {
-    try {
-      // Fire and forget...but handle errors
-      events.processEvent(cloudevent).catch((e) => {
-        log.error('erorr processing event: %j', cloudevent);
-        log.error(e);
-      });
-
-      return {
-        code: 202
-      };
-    } catch (e) {
-      log.error('error processing event: %j', cloudevent);
+    // Fire and forget...but handle errors
+    events.processEvent(cloudevent).catch((e) => {
+      log.error('erorr processing event: %j', cloudevent);
       log.error(e);
-      return {
-        statusCode: 500,
-        body: {
-          info: `error processing "${cloudevent.type}" event`
-        }
-      };
-    }
-  } else {
+    });
+
     return {
-      statusCode: 400,
-      body: {
-        info: `invalid cloud event type: "${cloudevent.type}"`
-      }
+      code: 202
+    };
+  } else {
+    log.warn(`an unknown event type of "${cloudevent.type}" was received`);
+    return {
+      code: 202
     };
   }
 };
